939dfa64e220c60bf5b2ac33284f12a04aaa1239
Vaibhav <sukalevaibhav53@gmail.com>
2024-03-02 17:42:04 +0530

    changes in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,39 +1,33 @@
 import React from "react";
 const About = () => {
-    return (
-      <div
-        name="about"
-        className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
-      >
-        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-          <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-              About
-            </p>
-          </div>
-  
-          <p className="text-xl mt-20">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-            deserunt illum mollitia officiis qui exercitationem perferendis neque
-            quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-            iste, totam fugiat recusandae rerum laborum perferendis molestiae
-            aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-            ab mollitia voluptatibus, a nostrum eveniet laborum!
-          </p>
-  
-          <br />
-  
-          <p className="text-xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-            pariatur, vel similique sint, nobis aspernatur ut praesentium
-            explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-            quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-            Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-            magni quo! Eum cupiditate debitis labore.
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
           </p>
         </div>
+
+        <p className="text-xl mt-20">
+          Hello, I'm Vaibhav, a final-semester Computer Science student with a
+          consistent CGPA above 8.5. I'm deeply passionate about coding and web
+          development, constantly exploring new technologies and frameworks to
+          create innovative solutions.
+        </p>
+        <br />
+        <p className="text-xl">
+          When I'm not immersed in code, you can find me on the cricket field,
+          where I enjoy the thrill of competition and teamwork. Off the field, I
+          unwind by diving into the captivating worlds of video games, where
+          strategic thinking and quick reflexes keep me engaged and entertained.
+        </p>
       </div>
-    );
-  };
-  
-  export default About;
+    </div>
+  );
+};
+
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,12 +13,11 @@ const Home = () => {
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
           <div className="flex flex-col justify-center h-full">
             <h2 className="text-4xl sm:text-7xl font-bold text-white">
-              I'm a Full Stack Developer
+              I'm a Frontend Developer and Programmer.
             </h2>
             <p className="text-gray-500 py-4 max-w-md">
-              I have 8 years of experience building and desgining software.
-              Currently, I love to work on web application using technologies like
-              React, Tailwind, Next JS and GraphQL.
+              I love to work on web application using technologies like
+              React, Tailwind,HTML,CSS and JavaScript.
             </p>
   
             <div>
